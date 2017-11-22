88779597a317ab5dc089fce9ca4c5c311e0e7750
Michael Pope <popemray1986@gmail.com>
2017-11-22 03:58:52 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  word_array = []
+
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word_array)
+    word.each do |w|
+      w = word%w(word_array)
+    end
+      
+
+
+  end
+
+
+
+end
