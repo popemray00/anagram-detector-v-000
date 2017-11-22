dc96b45f9ad52c308f6e7a0b527d03bf9ef12b40
Michael Pope <popemray1986@gmail.com>
2017-11-22 04:03:24 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,16 +2,17 @@
 class Anagram
   attr_accessor :word
 
-  word_array = []
 
 
   def initialize(word)
     @word = word
   end
 
-  #def match(word_array)
-  ##    w = word%w(word_array)
-    #end
+  def match(array)
+    array.select do |w|
+     w.split("").sort == word.split("").sort
+    end
+  end
 
 
 
